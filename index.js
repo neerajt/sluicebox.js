b8dfd0413fdcf1d7f9524023f97cc461fd181a96
Neeraj Tandon <neerajtandon25@gmail.com>
2014-06-01 00:18:44 -0400

    making a joad

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var util = require('util'),
-    twitter = require('twitter');
+    twitter = require('twitter'),
+    _ = require('lodash');
 var twit = new twitter({
     consumer_key: 'Uui4NrkShX9x8zFoAENiggPlN',
     consumer_secret: 'eIVwXceI57fJw3RWBxCOQRbFlEYS2W7zL7Q9P9l6yUgQxJzFt9',
@@ -7,12 +8,33 @@ var twit = new twitter({
     access_token_secret: 'DM8OhkJPSObjsO2At0935PiJlizNqSJEIExYVAo7cHnp2'
 });
 
-twit.stream('statuses/filter', {track:'foo'}, function(stream) {
+twit.stream('statuses/filter', {track:'manu'}, function(stream) {
     stream.on('data', function(data) {
-        console.log(util.inspect(data));
+        if(!data.retweeted_status){
+ 								var joad = {};
+        	if(data.entities.media && !(_.isUndefined(_.pluck(data.entities.media, 'media_url')))) {
+        		joad['story'] = (data.text);
+        		joad['image'] = _.pluck(data.entities.media, 'media_url')[0];
+        		joad['location'] = {'time':data.created_at, 'latitude':0, 'longitude':23};
+        		console.log(joad);
+        	};
+       	};
     });
+
     // Disconnect stream after five seconds
-    setTimeout(stream.destroy, 15000);
+    setTimeout(stream.destroy, 120000);
 });
 
+
+// {
+//     "name" : "Jane Doe",
+//     "tags" : [ "homeless", "devloper", "veteran" ],
+//     "story" : "Jane worked for Silicon Graphics, and then was foreced out of her house.",
+//     "location" : {
+//                     "time" : <ISO 8601 timestamp>,
+//                     "latitude" : -54.34532323,
+//                     "longitude" : 23.4242122
+//                  }
+//     "image" : "http://localhost:3000/sad_sgi_engineer.jpeg"
+// }
 
