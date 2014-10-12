14ad120e5c629275cf60cb148f7e98e3feefe084
neerajt <neerajtandon25@gmail.com>
2014-10-12 01:56:01 -0400

    I got a story to tell

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //setting shit up
   var util = require('util'),
       twitter = require('twitter'),
-      fs = require('fs'),
       _ = require('lodash'),
       http = require("http"),
       fs = require('fs');
@@ -9,18 +8,15 @@
 //read config stuff
   var opt = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
+  var st = JSON.parse(fs.readFileSync('search-terms.json', 'utf8'));
+
 // open twitter stream
   var twit = new twitter(opt);
 
 //make a tweetline
   function makeEntry(data){
-    var tweetline = {};
-    tweetline['story'] = (data.text);
-    tweetline['imageUrl'] = _.pluck(data.entities.media, 'media_url')[0];
-    tweetline['location'] = {'time':(new Date(data.created_at)).toISOString(), 'latitude':5.0, 'longitude':23.0};
-    tweetline['tags'] = ['tweetlines'];
     // tweetline['tags'] = _.pluck(data.entities.hashtags[0], 'text');
-    var strtweetline = JSON.stringify(tweetline);
+    var strtweetline = JSON.stringify(data);
     console.log(strtweetline);
     console.log(strtweetline.length);
     return strtweetline;
@@ -37,7 +33,7 @@
   });
 }
 
-twit.stream('statuses/filter', {track:'depression, depressed'}, function(stream) {
+twit.stream('statuses/filter', {track:st.search_terms}, function(stream) {
     stream.on('data', function(data) {
         if(!data.retweeted_status){
           if(data.entities.media && !(_.isUndefined(_.pluck(data.entities.media, 'media_url')))) {
@@ -49,6 +45,6 @@ twit.stream('statuses/filter', {track:'depression, depressed'}, function(stream)
     });
 
     // Disconnect stream after five seconds
-    setTimeout(stream.destroy, 120000);
+    //setTimeout(stream.destroy, 120000);
 });
 
